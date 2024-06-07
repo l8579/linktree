e8e7191b47a3f4a6523f0ef42a187ac105661d9f
l8579 <thanyanankhanchanan50@gmai.com>
2024-06-07 18:02:08 +0700

    update linktree

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LinkTreeApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LinkTreeApp from './views/LinkTreeApp.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LinkTreeApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Kanit', sans-serif;
 }
 </style>
---
+++ src/views/LinkTreeApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>พิกัดสินค้า</h1>
+        <p>Coming Soon...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LinkTreeApp'
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
